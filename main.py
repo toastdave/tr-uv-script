52d253477d165b19fd2bbc361cf4e937ef4a1718
dave <pie2ce@gmail.com>
2025-05-08 02:06:09 -0400

    testing out uv scripts - first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+def main():
+    womboNumby = random.randint(0,100000) * random.randint(0,100000)
+    print(f"wombo numby = {womboNumby}")
+
+
+if __name__ == "__main__":
+    main()
